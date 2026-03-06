73fcd5bbd9b532f879581d6f171a29c9beedbd09
viniciusg-ux <vinicius.haag@estudante.ifms.edu.br>
2026-03-06 11:43:55 -0400

    atividades feitas

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { cursorTo } from "readline"
+//mport { cursorTo } from "readline"
 
 let numero:number = 10
 let palavra:String = "TERE"
@@ -90,35 +90,68 @@ function somaPar(b:number[]):number{
 
 console.log(`o valor da somapar é ${somaPar([1,2,3])}`)
 
-//faça um função receba dois vetores e retorna 
-//um novo vetor com todos os valores passado.
-//exemplos func merge ([123]),([456]) retorne [1,2,3,4,5,6]
+/* 
+ * Exercício 01 - cria um novo vetor com os valores do vetor original mais dois novos valores
+ * Nome da função - criaNovoVetor
+ * Crie uma função que retorne um novo vetor com os valores do vetor original mais dois novos valores
+ * @param {number[]} vetor Vetor de números
+ * @param {number} valor1 Prim-eiro valor a ser adicionado
+ * @param {number} valor2 Segundo valor a ser adicionado
+ * @returns {number[]} Retorna um novo vetor com os valores do vetor original mais dois novos valores
+ * @example
+ * criaNovoVetor([1, 2, 3], 4, 5) // [1, 2, 3, 4, 5]
+ * criaNovoVetor([1, 2, 3], 0, 0) // [1, 2, 3, 0, 0]
+ /* 
+*/
+//Início do seu código
 
- function merge(A:number[], B:number[]):number{
-    let resultado =[], A = 0, B = 0;
-    while
- }
 
+function criaNovoVetor(v:number[], Valor1:number, Valor2:number):number[]{
+    numero = 0;
+    const resultado:number[] = []
 
-
-
-   
-
-
-
-function merge(left, right) {
-  let resultArray = [], leftIndex = 0, rightIndex = 0;
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      resultArray.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      resultArray.push(right[rightIndex]);
-      rightIndex++;
+    for( let i = 0; i < v.length; i++){
+        resultado.push(v[i]!)
     }
-  }
-  return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    resultado.push(Valor1)
+    resultado.push(Valor2)
+    
+    return resultado
 }
+
+
+const Valor1 = criaNovoVetor([1, 2, 3], 4, 5)
+console.log(`Novo vetor é: ${Valor1}`)
+
+const Valor2 = criaNovoVetor([6, 7, 8], 9 ,10)
+console.log(`Novo vetor é: ${Valor2}`)
+const resultado = criaNovoVetor([1, 2, 3, 4, 5], 6, 7)
+console.log(`Novo vetor é: ${resultado}`)
+
+
+console.log(`Novo vetor é: ${Valor2}`)
+
+
+//Fim do seu código
+
+/*
+ * Exercício 02 - divisivelPor11
+ * Nome da função - divisivelPor11
+ * Crie uma função que retorna um array com os números divisíveis por 11 no intervalo
+ * @param {number} min Número mínimo
+ * @param {number} max Número máximo
+ * @returns {number[]} Retorna um array com os números divisíveis por 11 no intervalo
+ * @example
+ *  divisivelPor11(1, 100) // [11, 22, 33, 44, 55, 66, 77, 88, 99]
+ *  
+ * divisivelPor11(11, 110) // [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+ */
+
+//Início do seu código
+
+
+//Fim do seu código
+
 
 
 
